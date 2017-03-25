6198b7a1e165fff5c71cb849597cb0d43279f869
Yara Mohammed <yara.mohammed174@gmail.com>
2017-03-25 04:18:48 +0200

    add object jquery

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,17 +1,43 @@
 products = []
+productsIds = []
+t = 0 ;
 
 jQuery(document).ready(function($) {
 
-  $(".allProducts").click(function (){
-    productId = this.id
-    if(products.includes(productId)){
-      console.log("already there");
-      $("#msg").text("*already added")
+
+  $("body").on('click' , '.allProducts' , (function (){
+    productId = this.id;
+    name = $("#name_"+this.id).text();
+    price = Number(this.alt);
+    if(productsIds.includes(productId)){
+      $("#msg_"+this.id).text("*already added")
     }
     else{
-      products.push(productId);
-      console.log("add to order "+productId);
-      console.log("update total");
+      var product = {"id":productId,"price":price,"name":name}
+      products.push(product);
+      productsIds.push(productId);
+        total = product['price'] ;
+        $("#items").append(
+          '<tr title="' + product['name'] + '" data-id="' + product['id'] + '" data-price="' + product['Price'] + '">' +
+          '<td>' + product['name'] + '</td>' +
+          '<td title="Unit Price">' + product['price'] + '</td>' +
+          '<td title="Quantity"><input type="number" class="quantity" min="1" value=1 style="width: 30px;" id=' + product['id'] + '_q value=""/></td>' +
+          '<td title="Total" id=' + product['id'] + '_t>$' + total + '</td>' +
+          '<td title="Remove from Cart" class="text-center" style="width: 40px;"><a href="javascript:void(0);" class="btn btn-xs btn-danger ' + "classProductRemove" + '">X</a></td>' +
+          '</tr>'
+        );
+        total = product['price']*Number($("#"+product['id']+"_q").val());
+        $("#"+product['id']+"_t").text(total);
     }
-  });
+  }));
+
+
+ $(document).on("input", ".quantity", function () {
+  var id = this.id.split("_")[0];
+  var price = Number($(this).closest("td").prev('td').text());
+  var totalThis = this.value * price ;
+  $("#"+id+"_t").text(totalThis) ;
+  t += totalThis ;
+  $("#total").text(t);
+    });
 });
