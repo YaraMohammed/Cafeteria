9b449b5f86aa9e2028a50379efdc3c678d262322
manar ahmed <manarahmedshehata@gmail.com>
2017-03-27 15:08:00 +0200

    Add each order products in admin order

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
 
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   #USERS ROUTES
   # get 'users' =>'users#index'
   # get 'users/new'
@@ -11,21 +11,30 @@ Rails.application.routes.draw do
   #PRODUCTS ROUTES
   # get 'products' =>'products#index'
   # get 'products/new'
+  
   # post 'products' => 'products#create'
   resources :products, only: [:index, :new, :create, :edit, :update, :destroy]
   post 'products/changestatus' =>'products#changestatus'
+
+
+
   #CATEGORIES ROUTES
   get 'categories/new'
   post 'categories' => 'categories#create'
+  
   #ORDER ROUTES
   get 'orders/new' =>'orders#new'
   get 'orders/myorder'
+  post 'orders/orderproductlist' => 'orders#orderproductlist'
+
   #post 'orders/myorder' =>'orders#list'
   post 'orders/new' =>'orders#create'
   get 'orders' =>'orders#list'
   post 'orders/deliver' =>'orders#deliver'
+  
   #SESSION ROUTES
   resources :sessions, only: [:new, :create, :destroy]
+  
   #login and logout
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
--- db/migrate/20170326101353_add_status_to_order.rb
+++ db/migrate/20170326101353_add_status_to_order.rb
@@ -1,5 +1,5 @@
 class AddStatusToOrder < ActiveRecord::Migration[5.0]
   def change
-    add_column :orders, :status, :string, default: "deliver"
+    add_column :orders, :status, :string, default: "Processing"
   end
 end
